f7d66355b8388b2945220d2b7c47e04a22ed1de6
kikisad <killiancartignies@gmail.com>
2021-03-05 09:40:51 +0100

    ksqdjhkqjshd

--- components/post-body.js
+++ components/post-body.js
@@ -1,7 +1,7 @@
 import markdownStyles from './markdown-styles.module.css';
 export default function PostBody({ children }) {
   return (
-    <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-4xl mx-auto px-5 md:px-0 md:text-justify font-normal text-xs textxxs sm:text-sm leading-2 sm:text-lg sm:leading-7 lg:text-text-left	">
+    <div className="prose prose-lg max-w-5xl mx-auto px-5 md:px-8 md:text-justify font-normal text-xs textxxs sm:text-sm leading-2 sm:text-lg sm:leading-7 lg:text-text-left	">
       <div className={markdownStyles['markdown']}>{children}</div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
-    
+
     extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
             color: theme('colors.gray.800'),
+
           },
-          
         },
       }),
     }
